b2a3baa779b3af6d23a1bc90feccd870b9c143a7
maxmheinze <heinzemax@icloud.com>
2023-04-04 15:56:30 +0200

    assignment 1 question 1, creating the function and the first two plots

---
+++ assignment1/code/assignment1_question1.R
@@ -0,0 +1,94 @@
+
+# Header ------------------------------------------------------------------
+
+rm(list = ls())
+gc()
+
+pacman::p_load(
+  tidyverse
+  )
+
+
+# Read in Data ------------------------------------------------------------
+
+fred <- read.csv("./assignment1/data/fred.csv")[-1,]
+
+
+# Create the function -----------------------------------------------------
+
+ts_explode <- function(input_vector, start_with_latest = FALSE) {
+  # The function is called ts_explode because a single vector explodes into an entire data frame. Boom!
+  
+  # Package dplyr required for lag() function
+  require(dplyr)
+  
+  # Reverse input vector if user specifies it is sorted latest to earliest
+  input_vector <- if (start_with_latest == FALSE) {
+    input_vector
+  } else {
+    rev(input_vector)
+  } 
+  
+  # Do the transformations, assign transformed vectors
+  original <- input_vector
+  log_transformed <- log(input_vector)
+  mom_growth <- input_vector/dplyr::lag(input_vector, 1) - 1
+  yoy_growth <- input_vector/dplyr::lag(input_vector, 12) - 1
+  yoy_growth_lagged <- dplyr::lag(input_vector, 12)/dplyr::lag(input_vector, 24) - 1
+  
+  # Create a data frame to export, reverse ordering back to original in case start_with_latest = TRUE was specified
+  export_df <- if (start_with_latest == FALSE) {
+    data.frame(original, log_transformed, mom_growth, yoy_growth, yoy_growth_lagged)
+  } else {
+    data.frame(original = rev(original), 
+               log_transformed = rev(log_transformed), 
+               mom_growth = rev(mom_growth), 
+               yoy_growth = rev(yoy_growth), 
+               yoy_growth_lagged = rev(yoy_growth_lagged))
+  }
+  
+  # Display warnings regarding ordering and units of growth rates
+  warning("By default, ts_explode() assumes that values are ordered from earliest to latest. If your vector is ordered from latest to earliest, specify `start_with_latest = TRUE`!")
+  warning("Growth rates are given in decimals, not in percent!")
+  
+  # Return the data frame
+  return(export_df)
+}
+
+
+
+# Prepare Industrial Production Data Frame --------------------------------
+
+ind_prod <- fred$sasdate %>%
+  cbind(ts_explode(fred$INDPRO)) %>%
+  as_tibble() %>%
+  mutate(date = lubridate::mdy(`.`)) %>%
+  select(-`.`) %>%
+  relocate(date, .before = original)
+
+
+# Create Log Plot ---------------------------------------------------------
+
+ind_prod %>%
+  ggplot() +
+  geom_line(aes(x = date, y = log_transformed)) +
+  labs(title = "U.S. Industrial Production (logged)",
+       x = "Date",
+       y = "Log of Industrial Production") +
+  ylim(3,5) +
+  theme_bw()
+
+ggsave("./assignment1/output/plot_logged.png", plot = last_plot(), width = 6, height = 4)
+
+
+# Create Growth Plot ------------------------------------------------------
+
+ind_prod %>%
+  ggplot() +
+  geom_line(aes(x = date, y = yoy_growth)) +
+  labs(title = "U.S. Industrial Production (year-on-year growth)",
+       x = "Date",
+       y = "Year-on-Year Growth of Industrial Production") +
+  theme_bw()
+
+ggsave("./assignment1/output/plot_yoy.png", plot = last_plot(), width = 6, height = 4)
