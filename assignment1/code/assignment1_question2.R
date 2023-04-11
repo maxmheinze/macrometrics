5d1dcb21fe97447666b311a25a927f834f24ad62
Tim Koenders <koenders.tim@hotmail.com>
2023-04-11 16:06:35 +0200

    Estimated the VAR and IRFS. Next step: Replicating figure 1 and 3

--- assignment1/code/assignment1_question2.R
+++ assignment1/code/assignment1_question2.R
@@ -16,3 +16,16 @@ kpdata <- read.table("./assignment1/data/data_kilian_park_2009.txt", header = FA
 colnames(kpdata) <- c("oil_prod_change", "econ_act_real", "oil_price_real", "div_growth_change_real")
 
 # Estimating the VAR ------------------------------------------------------
+
+var_model <- VAR(kpdata, p = 1, type = "const")
+coefficients <- coef(var_model)
+residuals <- resid(var_model)
+print(coefficients)
+
+# Computing impulse response function
+
+irf1 <- irf(var_model, impulse = "oil_price_real", response = "oil_prod_change", n.ahead = 15)
+irf2 <- irf(var_model, impulse = "oil_price_real", response = "econ_act_real", n.ahead = 15)
+irf3 <- irf(var_model, impulse = "oil_price_real", response = "oil_price_real", n.ahead = 15)
+
+
