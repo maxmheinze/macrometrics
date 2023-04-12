10cc2b2da9c4095cf34c041c90c4e344d0b10fe0
Siggi Hammer <siggi@Siggis-MacBook-Air.local>
2023-04-12 14:51:58 +0200

    changed lag order and irf plots

--- assignment1/code/assignment1_question2.R
+++ assignment1/code/assignment1_question2.R
@@ -17,15 +17,18 @@ colnames(kpdata) <- c("oil_prod_change", "econ_act_real", "oil_price_real", "div
 
 # Estimating the VAR ------------------------------------------------------
 
-var_model <- VAR(kpdata, p = 1, type = "const")
+var_model <- VAR(kpdata, p = 24, type = "const")
 coefficients <- coef(var_model)
 residuals <- resid(var_model)
 print(coefficients)
 
 # Computing impulse response function
 
-irf1 <- irf(var_model, impulse = "oil_price_real", response = "oil_prod_change", n.ahead = 15)
-irf2 <- irf(var_model, impulse = "oil_price_real", response = "econ_act_real", n.ahead = 15)
+irf1 <- irf(var_model, impulse = "oil_prod_change", response = "oil_price_real", n.ahead = 15)
+irf2 <- irf(var_model, impulse = "econ_act_real", response = "oil_price_real", n.ahead = 15)
 irf3 <- irf(var_model, impulse = "oil_price_real", response = "oil_price_real", n.ahead = 15)
 
-
+#Plotting the IRFs to recreate Figure 1
+plot(irf1, main="Oil supply shock")
+plot(irf2, main="Aggregate demand shock")
+plot(irf2, main="Oil-specific demand shock")
