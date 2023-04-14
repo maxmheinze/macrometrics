3fbd27f444798a347917d361c5610e8541ea23ca
maxmheinze <heinzemax@icloud.com>
2023-04-15 00:39:50 +0200

    completed try at question 2

--- assignment1/code/assignment1_question2.R
+++ assignment1/code/assignment1_question2.R
@@ -22,7 +22,7 @@ coefficients <- coef(var_model)
 residuals <- resid(var_model)
 print(coefficients)
 
-summary(var_model) #checked
+summary(var_model)
 
 # Computing impulse response function
 
@@ -41,7 +41,7 @@ plot(irf1, main="Oil supply shock", ylab="Real price of oil")
 
 plot(irf2, main="Aggregate demand shock", ylab="Real price of oil")
 
-plot(irf3, main="Oil-specific demand shock", ylab="Real price of oil") # NOT CORRECT
+plot(irf3, main="Oil-specific demand shock", ylab="Real price of oil") 
 
 
 
@@ -106,7 +106,74 @@ cpi_kp <- ts_explode(fred_kp$CPIAUCSL) %>%
 
 real_stock_returns <- sp_kp$sp500_momlog-cpi_kp$cpi_mom
 
-kpdata_2 <- cbind(kpdata, 100*real_stock_returns)
+kpdata_2 <- cbind(kpdata, real_stock_returns = 100*real_stock_returns)
 
 
-# Does this make sense?? 
+
+# Re-estimating the model -------------------------------------------------
+
+
+var_model_2 <- VAR(kpdata_2, p = 24, type = "const")
+
+print(coefficients)
+
+summary(var_model_2)
+
+
+
+# Replicating Figure 1 ----------------------------------------------------
+
+irf7 <- irf(var_model_2, impulse = "oil_prod_change", response = "oil_price_real", n.ahead = 15)
+irf7$irf$oil_prod_change <- irf7$irf$oil_prod_change * (-1) # Switching signs of shock
+irf7$Lower$oil_prod_change <- irf7$Lower$oil_prod_change * (-1) # and of the CIs
+irf7$Upper$oil_prod_change <- irf7$Upper$oil_prod_change * (-1)
+
+plot(irf7, ylim = c(-6,12), main="Oil supply shock", ylab="Real price of oil")
+
+
+irf8 <- irf(var_model_2, impulse = "econ_act_real", response = "oil_price_real", n.ahead = 15)
+
+plot(irf8, ylim = c(-6,12),  main="Aggregate demand shock", ylab="Real price of oil")
+
+
+irf9 <- irf(var_model_2, impulse = "oil_price_real", response = "oil_price_real", n.ahead = 15)
+
+plot(irf9, ylim = c(-6,12), main="Oil-specific demand shock", ylab="Real price of oil")
+
+
+# Replicating Figure 3 ----------------------------------------------------
+
+irf10 <- irf(var_model_2, impulse = "oil_price_real", response = "real_stock_returns", 
+            cumulative = TRUE, n.ahead = 15)
+
+plot(irf10, ylim = c(-3,3), main="Oil-specific demand shock", ylab="Cumulative Real Dividends (Percent)")
+
+
+irf11 <- irf(var_model_2, impulse = "econ_act_real", response = "real_stock_returns", 
+            cumulative = TRUE, n.ahead = 15)
+
+plot(irf11, ylim = c(-3,3),  main="Aggregate demand shock", ylab="Cumulative Real Dividends (Percent)")
+
+
+irf12 <- irf(var_model_2, impulse = "oil_prod_change", response = "real_stock_returns", 
+            cumulative = TRUE, n.ahead = 15)
+
+irf12$irf$oil_prod_change <- (-1)*irf12$irf$oil_prod_change # Switching signs of shock
+irf12$Lower$oil_prod_change <- (-1)*irf12$Lower$oil_prod_change # and of the CIs
+irf12$Upper$oil_prod_change <- (-1)*irf12$Upper$oil_prod_change
+
+plot(irf12, ylim = c(-3,3), main="Oil supply shock", ylab="Cumulative Real Dividends (Percent)")
+
+# Replicating Table 1 -----------------------------------------------------
+
+table_1 <- fevd(var_model_2, n.ahead = 1000)$real_stock_returns %>%
+  `*`(100) %>%
+  cbind(horizon = 1:1000) %>%
+  as_tibble() %>%
+  dplyr::relocate(horizon, .before = oil_prod_change) %>%
+  dplyr::select(-div_growth_change_real) %>%
+  dplyr::filter(horizon %in% c(1, 2, 3, 12, 1000)) %>%
+  mutate(horizon = ifelse(horizon == 1000, Inf, horizon))
+
+table_1
+
