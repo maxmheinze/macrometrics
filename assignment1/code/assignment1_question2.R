c456f346b2982a2ab0d28529359a2b30961bb461
maxmheinze <heinzemax@icloud.com>
2023-04-15 01:41:44 +0200

    updated assignment 2, included it in the Rmd

--- assignment1/code/assignment1_question2.R
+++ assignment1/code/assignment1_question2.R
@@ -47,33 +47,32 @@ plot(irf3, main="Oil-specific demand shock", ylab="Real price of oil")
 
 # Replicating Figure 3 ----------------------------------------------------
 
-irf4 <- irf(var_model, impulse = "oil_price_real", response = "div_growth_change_real", 
+irf4 <- irf(var_model, impulse = "oil_prod_change", response = "div_growth_change_real", 
             cumulative = TRUE, n.ahead = 15)
 
-plot(irf4, ylim = c(-3,3), main="Oil-specific demand shock", ylab="Cumulative Real Dividends (Percent)")
-
+irf4$irf$oil_prod_change <- (-1)*irf4$irf$oil_prod_change # Switching signs of shock
+irf4$Lower$oil_prod_change <- (-1)*irf4$Lower$oil_prod_change # and of the CIs
+irf4$Upper$oil_prod_change <- (-1)*irf4$Upper$oil_prod_change
 
 irf5 <- irf(var_model, impulse = "econ_act_real", response = "div_growth_change_real", 
             cumulative = TRUE, n.ahead = 15)
 
-plot(irf5, ylim = c(-3,3),  main="Aggregate demand shock", ylab="Cumulative Real Dividends (Percent)")
-
-
-irf6 <- irf(var_model, impulse = "oil_prod_change", response = "div_growth_change_real", 
+irf6 <- irf(var_model, impulse = "oil_price_real", response = "div_growth_change_real", 
             cumulative = TRUE, n.ahead = 15)
 
-irf6$irf$oil_prod_change <- (-1)*irf6$irf$oil_prod_change # Switching signs of shock
-irf6$Lower$oil_prod_change <- (-1)*irf6$Lower$oil_prod_change # and of the CIs
-irf6$Upper$oil_prod_change <- (-1)*irf6$Upper$oil_prod_change
 
-plot(irf6, ylim = c(-3,3), main="Oil supply shock", ylab="Cumulative Real Dividends (Percent)")
+plot(irf4, ylim = c(-3,3), main="Oil supply shock", ylab="Cumulative Real Dividends (Percent)")
+
+plot(irf5, ylim = c(-3,3),  main="Aggregate demand shock", ylab="Cumulative Real Dividends (Percent)")
+
+plot(irf6, ylim = c(-3,3), main="Oil-specific demand shock", ylab="Cumulative Real Dividends (Percent)")
 
 
 
 # Replicating Table 2 -----------------------------------------------------
 
 table_2 <- fevd(var_model, n.ahead = 1000)$div_growth_change_real %>%
-  `*`(100) %>%
+  magrittr::multiply_by(100) %>%
   cbind(horizon = 1:1000) %>%
   as_tibble() %>%
   dplyr::relocate(horizon, .before = oil_prod_change) %>%
@@ -95,13 +94,13 @@ fred_kp <- fred[169:576,] %>% # only choosing 1.1973 to 12.2006
 
 
 sp_kp <- ts_explode(fred_kp$S.P.500) %>%
-  `colnames<-`(c("sp500", "sp500_log", "sp500_mom", "sp500_momlog", "sp500_yoy", "sp500_yoylag")) %>%
-  `[`(-1,) %>% # Kilian&Park data set has only 407 rows so maybe they also omitted the first?
+  magrittr::set_colnames(c("sp500", "sp500_log", "sp500_mom", "sp500_momlog", "sp500_yoy", "sp500_yoylag")) %>%
+  magrittr::extract(-1,) %>% # Kilian&Park data set has only 407 rows so maybe they also omitted the first?
   as_tibble()
 
 cpi_kp <- ts_explode(fred_kp$CPIAUCSL) %>%
-  `colnames<-`(c("cpi", "cpi_log", "cpi_mom", "cpi_momlog", "cpi_yoy", "cpi_yoylag")) %>%
-  `[`(-1,) %>%
+  magrittr::set_colnames(c("cpi", "cpi_log", "cpi_mom", "cpi_momlog", "cpi_yoy", "cpi_yoylag")) %>%
+  magrittr::extract(-1,) %>%
   as_tibble()
 
 real_stock_returns <- sp_kp$sp500_momlog-cpi_kp$cpi_mom
@@ -128,46 +127,44 @@ irf7$irf$oil_prod_change <- irf7$irf$oil_prod_change * (-1) # Switching signs of
 irf7$Lower$oil_prod_change <- irf7$Lower$oil_prod_change * (-1) # and of the CIs
 irf7$Upper$oil_prod_change <- irf7$Upper$oil_prod_change * (-1)
 
-plot(irf7, ylim = c(-6,12), main="Oil supply shock", ylab="Real price of oil")
-
-
 irf8 <- irf(var_model_2, impulse = "econ_act_real", response = "oil_price_real", n.ahead = 15)
 
-plot(irf8, ylim = c(-6,12),  main="Aggregate demand shock", ylab="Real price of oil")
-
-
 irf9 <- irf(var_model_2, impulse = "oil_price_real", response = "oil_price_real", n.ahead = 15)
+
+
+plot(irf7, ylim = c(-6,12), main="Oil supply shock", ylab="Real price of oil")
+
+plot(irf8, ylim = c(-6,12),  main="Aggregate demand shock", ylab="Real price of oil")
 
 plot(irf9, ylim = c(-6,12), main="Oil-specific demand shock", ylab="Real price of oil")
 
 
 # Replicating Figure 3 ----------------------------------------------------
 
-irf10 <- irf(var_model_2, impulse = "oil_price_real", response = "real_stock_returns", 
-            cumulative = TRUE, n.ahead = 15)
+irf10 <- irf(var_model_2, impulse = "oil_prod_change", response = "real_stock_returns", 
+             cumulative = TRUE, n.ahead = 15)
 
-plot(irf10, ylim = c(-3,3), main="Oil-specific demand shock", ylab="Cumulative Real Dividends (Percent)")
-
+irf10$irf$oil_prod_change <- (-1)*irf10$irf$oil_prod_change # Switching signs of shock
+irf10$Lower$oil_prod_change <- (-1)*irf10$Lower$oil_prod_change # and of the CIs
+irf10$Upper$oil_prod_change <- (-1)*irf10$Upper$oil_prod_change
 
 irf11 <- irf(var_model_2, impulse = "econ_act_real", response = "real_stock_returns", 
-            cumulative = TRUE, n.ahead = 15)
+             cumulative = TRUE, n.ahead = 15)
+
+irf12 <- irf(var_model_2, impulse = "oil_price_real", response = "real_stock_returns", 
+             cumulative = TRUE, n.ahead = 15)
+
+
+plot(irf10, ylim = c(-3,3), main="Oil supply shock", ylab="Cumulative Real Dividends (Percent)")
 
 plot(irf11, ylim = c(-3,3),  main="Aggregate demand shock", ylab="Cumulative Real Dividends (Percent)")
 
-
-irf12 <- irf(var_model_2, impulse = "oil_prod_change", response = "real_stock_returns", 
-            cumulative = TRUE, n.ahead = 15)
-
-irf12$irf$oil_prod_change <- (-1)*irf12$irf$oil_prod_change # Switching signs of shock
-irf12$Lower$oil_prod_change <- (-1)*irf12$Lower$oil_prod_change # and of the CIs
-irf12$Upper$oil_prod_change <- (-1)*irf12$Upper$oil_prod_change
-
-plot(irf12, ylim = c(-3,3), main="Oil supply shock", ylab="Cumulative Real Dividends (Percent)")
+plot(irf12, ylim = c(-3,3), main="Oil-specific demand shock", ylab="Cumulative Real Dividends (Percent)")
 
 # Replicating Table 1 -----------------------------------------------------
 
 table_1 <- fevd(var_model_2, n.ahead = 1000)$real_stock_returns %>%
-  `*`(100) %>%
+  magrittr::multiply_by(100) %>%
   cbind(horizon = 1:1000) %>%
   as_tibble() %>%
   dplyr::relocate(horizon, .before = oil_prod_change) %>%
