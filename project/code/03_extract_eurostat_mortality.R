707fe3f3575bc92e6b67ed0b28164ca9739a4374
maxmheinze <heinzemax@icloud.com>
2023-07-22 17:49:12 +0200

    include eurostat mortality data

---
+++ project/code/03_extract_eurostat_mortality.R
@@ -0,0 +1,45 @@
+
+
+# Header ------------------------------------------------------------------
+
+pacman::p_load(
+  tidyverse,
+  countrycode,
+  readr,
+  lubridate
+)
+
+# Local file path since original file is too large to put on GitHub
+# Download Link for File from Eurostat:
+# https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/DEMO_R_MWK3_T/?format=SDMX-CSV&lang=en&label=both
+in_path <- "..."
+
+
+# Read Data ---------------------------------------------------------------
+
+eurostat_mortality <- read_csv(in_path)
+
+nuts_levels <- read_csv("./project/data/nuts_levels.csv")
+
+
+# Data Wrangling ----------------------------------------------------------
+
+mortality <- eurostat_mortality %>%
+  dplyr::select(geo, TIME_PERIOD, OBS_VALUE) %>%
+  separate(geo, into = c("nuts_code", "nuts_name"), sep = ":") %>%
+  separate(TIME_PERIOD, into = c("year", "week"), sep = "-W") %>%
+  rename(deaths = OBS_VALUE)
+
+mortality <- mortality %>%
+  left_join(nuts_levels, by = "nuts_code") %>%
+  relocate(nuts_level, .before = year) %>%
+  mutate(year = as.numeric(year),
+         week = as.numeric(week),
+         nuts_level = as.factor(nuts_level))
+
+
+# Write CSV ---------------------------------------------------------------
+
+write_csv(mortality, "./project/data/mortality.csv")
+
+
