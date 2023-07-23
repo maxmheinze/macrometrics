761131919afc2117c7169778f59eda2a672fc46a
Gustav Pirich <gustavpirich@149-130.vpnc.wu-wien.ac.at>
2023-07-23 19:18:41 +0800

    age-adjusted-mean

--- project/code/05_extract_eurostat_mortality_age.R
+++ project/code/05_extract_eurostat_mortality_age.R
@@ -11,13 +11,22 @@ pacman::p_load(
 
 # Local file path since original file is too large to put on GitHub
 # Download Link for File from Eurostat:
-# https://ec.europa.eu/eurostat/databrowser/view/DEMO_R_MWK3_10/default/table?lang=en
-in_path <- "/Users/gustavpirich/Library/Mobile Documents/com~apple~CloudDocs/Wirtschaftsuniversitaet/MASTER/Advanced Macroeconometrics/MacroMetrics/Project/raw/nuts/demo_r_mwk3_10_linear.csv"
+# https://ec.europa.eu/eurostat/databrowser/view/DEMO_R_MWK3_20/default/table?lang=en
+# https://ec.europa.eu/eurostat/databrowser/view/DEMO_R_GIND3__custom_6985794/default/table?lang=en
+# https://ec.europa.eu/eurostat/databrowser/view/DEMO_R_PJANIND3__custom_6985756/default/table?lang=en
+
+in_path_eurostat_mortality <- "/Users/gustavpirich/Library/Mobile Documents/com~apple~CloudDocs/Wirtschaftsuniversitaet/MASTER/Advanced Macroeconometrics/MacroMetrics/Project/raw/nuts/demo_r_mwk3_20_linear.csv"
+in_path_population <- "/Users/gustavpirich/Library/Mobile Documents/com~apple~CloudDocs/Wirtschaftsuniversitaet/MASTER/Advanced Macroeconometrics/MacroMetrics/Project/raw/nuts/demo_r_gind3__custom_6985794_linear.csv"
+in_path_population_structure <- "/Users/gustavpirich/Library/Mobile Documents/com~apple~CloudDocs/Wirtschaftsuniversitaet/MASTER/Advanced Macroeconometrics/MacroMetrics/Project/raw/nuts/demo_r_pjanind3__custom_6985756_linear.csv"
 
 
 # Read Data ---------------------------------------------------------------
 
-eurostat_mortality <- read_csv(in_path)
+eurostat_mortality <- read_csv(in_path_eurostat_mortality)
+
+population <- read_csv(in_path_population)
+
+population_structure <- read_csv(in_path_population_structure)
 
 nuts_levels <- read_csv("./project/data/nuts_levels.csv")
 
@@ -27,18 +36,37 @@ nuts_levels <- read_csv("./project/data/nuts_levels.csv")
 mortality <- eurostat_mortality %>%
   dplyr::select(geo, sex, age, TIME_PERIOD, OBS_VALUE) %>%
   dplyr::filter(sex == "T")  %>%
-  #separate(geo, into = c("nuts_code", "nuts_name"), sep = ":") %>%
   separate(TIME_PERIOD, into = c("year", "week"), sep = "-W") %>%
   rename(deaths = OBS_VALUE) %>%
   rename(nuts_code = geo)
 
 mortality <- mortality %>%
-  separate(geo, into = c("nuts_code", "nuts_name"), sep = ":") 
   left_join(nuts_levels, by = "nuts_code") %>%
   relocate(nuts_level, .before = year) %>%
   mutate(year = as.numeric(year),
          week = as.numeric(week),
          nuts_level = as.factor(nuts_level))
+
+
+
+
+
+# Population --------------------------------------------------------------
+
+population <- population %>%
+  select(geo, TIME_PERIOD, OBS_VALUE) %>%
+  rename(population = OBS_VALUE) %>%
+  rename(nuts_code = geo) %>%
+  left_join(nuts_levels, by = "nuts_code") 
+
+population_structure <- population_structure %>% 
+  select(indic_de, geo, TIME_PERIOD, OBS_VALUE) %>%
+  rename(nuts_code = geo) %>%
+  rename(pop_share = OBS_VALUE) %>%
+  left_join(nuts_levels, by = "nuts_code") %>%
+  mutate(pop_share = pop_share/100) %>%
+  left_join(population, by = c("nuts_code", "TIME_PERIOD", "nuts_level"))
+
 
 
 # Write CSV ---------------------------------------------------------------
