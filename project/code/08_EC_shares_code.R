f38e3b9c05ff769ea31822aa5916f42f7c45c5d3
Hannes <136262309+hanneswilko@users.noreply.github.com>
2023-07-24 16:59:38 +0200

    data transformation EC shares for HH

---
+++ project/code/08_EC_shares_code.R
@@ -0,0 +1,21 @@
+pacman::p_load(
+  dplyr,
+  tidyr,
+  countrycode
+)
+
+EC_shares_HH <- read.csv("/Users/hannes/Documents/Studium/Master/Second_Semester/MACROECO/project/project_1/project_firstdata/EC_shares.csv") %>%
+  filter(!siec=="TOTAL") %>%
+  rename(ccode = geo, year = TIME_PERIOD, energy_carrier = siec, values_TJ = OBS_VALUE) %>%
+  mutate(energy_carrier = recode(energy_carrier, E7000 = 'Electricity', G3000 = 'Natural gas', H8000 =  'Heat', O4000 =  'Oil and petroleum products',
+                       RA000 =  'Renewables and biofuels', SFF_P1000_S2000 =  'Solid fossil fuels'),
+         cname = countrycode(ccode, "eurostat", "country.name")) %>%
+  group_by(ccode,year) %>%
+  mutate(
+    shares = round(values_TJ / sum(values_TJ, na.rm = FALSE), 4)
+  ) %>%
+  ungroup() %>%
+  select(ccode,cname,year,energy_carrier,values_TJ,shares) %>%
+  arrange(ccode)
+
+write.csv(EC_shares_HH, file = "/Users/hannes/Documents/Studium/Master/Second_Semester/MACROECO/project/project_1/project_firstdata/EC_shares.csv", row.names = FALSE)
