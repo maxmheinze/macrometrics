f00f024f38a480011c9c22aed72ce0119903cd5b
Gustav Pirich <gustavpirich@Gustavs-MacBook-Pro-2.local>
2023-07-24 22:13:14 +0800

    new code

--- project/code/07_data_merge.R
+++ project/code/07_data_merge.R
@@ -23,4 +23,28 @@ temperature <- read_csv("./project/data/temperature_weekly.csv")
 
 # Data Merge --------------------------------------------------------------
 
-prices %>% 
+temp_mort <- temperature %>%
+  left_join(age_adjusted_weekly_mortality, by = c("nuts_code", "nuts_level", "week", "year"))
+
+prices_1 <- prices %>%
+  mutate(country = countrycode(prices$country_clean, "country.name", "eurostat"))
+
+temp_mort_prices <- temp_mort %>% 
+  mutate(country = substr(nuts_code, 1, 2)) 
+  #mutate(month = format(as.Date(paste(1, week, year), "%u %U %Y"), "%B"))
+  #left_join(prices_1, by = c("country", "year", "month"))
+
+temp_mort_prices %>%
+  mutate(
+    x = as.POSIXct(strptime(paste0(x, "-1"), format = "%Y-%W-%u")),
+    month = format(x, "%m"),
+    week = 1 + as.integer(format(, "%d")) %/% 7)
+
+# Replace NA values in the Date column with a specified date
+replacement_date <- as.Date("2020-12-31")
+euromomo_score$Date[is.na(euromomo_score$Date)] <- replacement_date
+  
+# Add month column
+euromomo_score$month <- month(euromomo_score$Date)
+  
+  
