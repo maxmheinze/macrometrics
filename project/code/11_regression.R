38e48921a36a9fed1edf094476a7b18a2c2c237f
Gustav Pirich <gustavpirich@149-130.vpnc.wu-wien.ac.at>
2023-07-26 19:58:44 +0800

    new variable

--- project/code/11_regression.R
+++ project/code/11_regression.R
@@ -24,14 +24,31 @@ data_temp <- temp_mort_prices %>%
   mutate(age_adjusted_mortality = age_adjusted_mortality*100000) %>%
   group_by(nuts_code, year, month, week)  %>% 
   mutate(date = as.factor(paste(year, month, week, sep = "-")))  %>% 
+  mutate(temperature = temperature + 273.15) %>%
   filter(!(dates >= as.Date("2020-03-01") & dates <= as.Date("2022-03-31"))) 
 
 
 # Regression --------------------------------------------------------------
 
-feols(age_adjusted_mortality ~ log(gas_ppi) + temperature +  temperature^2| nuts_code + dates, data_temp)
 
-feols(age_adjusted_mortality ~ log(gas_ppi) + temperature + temperature^2 + temperature*log(gas_ppi)| nuts_code + period, data_temp)
+
+# Effect of temperature on mortality --------------------------------------
+
+reg <-feols(age_adjusted_mortality ~ temperature +  temperature^2 | nuts_code + date, data_temp)
+
+lowest_mortality = (-coef(reg)["temperature"] / (2*coef(reg)["I(temperature^2)"])) - 273.15
+
+feols(age_adjusted_mortality ~ temperature +  temperature^2 + temperature^3 | nuts_code + date, data_temp)
+
+
+
+feols(age_adjusted_mortality ~ log(gas_ppi) + temperature + I(temperature^2) + log(gas_ppi):temperature + log(gas_ppi):I(temperature^2) | nuts_code + date, data_temp)
+
+
+feols(age_adjusted_mortality ~ l(log(gas_ppi),8) | nuts_code + date, data_temp %>% filter(month %in% c("11", "12", "1", "2")))
+
+
+feols(age_adjusted_mortality ~ log(gas_ppi) + temperature + temperature^2 + temperature*log(gas_ppi)| nuts_code + year + month  + week, data_temp)
 
 reg = lm(age_adjusted_mortality ~ date + nuts_code + gas_ppi, data_temp)
 
@@ -52,6 +69,20 @@ summary(reg_2)
 
 
 reg_3 = plm(age_adjusted_mortality ~  I(lag(log(gas_ppi),8L)^temperature), 
+            index = c("nuts_code", "date"),
+            effect = "twoways",
+            data = data_temp)
+
+summary(reg_3)
+
+
+# lagged regression with plm ----------------------------------------------
+
+pdata <- pdata.frame(data_temp, index = c("nuts_code","date"))
+
+pdata$lag_pcap <- lag(pdata$gas_ppi, 8)
+
+reg_3 = plm(age_adjusted_mortality ~ , 
             index = c("nuts_code", "date"),
             effect = "twoways",
             data = data_temp)
