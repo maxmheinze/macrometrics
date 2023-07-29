39d6be08833766696cd6e8b8950674d7b3b7c604
Gustav Pirich <gustavpirich@149-130.vpnc.wu-wien.ac.at>
2023-07-29 18:20:58 +0800

    new IV stuff

--- project/code/13_regression_IV.R
+++ project/code/13_regression_IV.R
@@ -12,8 +12,8 @@ pacman::p_load(
   plm, 
   nlme, 
   lme4, 
-  lmerTest 
-)
+  zoo, 
+  broom)
 
 source("./project/code/07_data_merge.R")
 
@@ -50,14 +50,56 @@ pdata <- na.omit(pdata, cols = "temp_bin")
 
 pdata$temp_bin <- relevel(pdata$temp_bin, ref = "10-15")
 
+
+
+# linear interpolation for electricity share of ec ------------------------
+
 ec_shares_hh_1 <- ec_shares_hh %>% 
   rename(nuts_code = ccode) %>% 
   filter(energy_carrier == "Natural gas") %>%
-  select(nuts_code, shares, year)
+  select(nuts_code, shares, year) 
 
-test <- pdata %>%
-  left_join(ec_shares_hh_1, by = c("nuts_code", "year")) 
+all_years <- 2017:2023
+
+# Expand the data frame
+df_full <- ec_shares_hh_1 %>%
+  tidyr::complete(nuts_code, year = all_years) %>%
+  group_by(nuts_code)
+
+predictions <- round(predict(lm(shares ~ year *nuts_code, df_full), newdata = df_full), 2)
+predictions 
+
+df_full$shares[is.na(df_full$shares)] <- predictions[is.na(df_full$shares)]
+
+df_full$shares <- pmax(df_full$shares, 0)
+
+final <- pdata %>%
+  left_join(df_full, by = c("nuts_code", "year")) 
+
+
+
+
 
 # Shift Share Regression --------------------------------------------------
+
+
+reg_1 = plm(age_adjusted_mortality ~ I(log(lag_pcap/lag_pecep)*shares), 
+            data = final, 
+            model = "within", 
+            effect = "twoways")
+summary(reg_1)
+
+reg_2 = plm(age_adjusted_mortality ~ I(log(lag_pcap/lag_pecep)*shares), 
+             data = (final %>% filter(month %in% c(11, 12, 1, 2, 3))), 
+             model = "within", 
+             effect = "twoways")
+summary(reg_2)
+
+
+reg_3 = plm(age_adjusted_mortality ~ I(log(lag_pcap/lag_pecep)*shares), 
+            data = final, 
+            model = "within", 
+            effect = "twoways")
+summary(reg_3)
 
 
