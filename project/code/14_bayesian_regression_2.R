d9a8e6ca7e09eba14295f04b26acb47006008a40
maxmheinze <heinzemax@icloud.com>
2023-08-04 00:17:59 +0200

    update script 14

--- project/code/14_bayesian_regression_2.R
+++ project/code/14_bayesian_regression_2.R
@@ -8,83 +8,8 @@ pacman::p_load(
   rstanarm
 )
 
-source("./project/code/07_data_merge.R")
 
-
-# Load Data, Subset Columns -----------------------------------------------
-
-full_data <- temp_mort_prices %>%
-  filter(!is.na(gas_ppi)) %>%
-  filter(!is.na(temperature)) %>%
-  filter(!is.na(age_adjusted_mortality))
-
-
-# Fit Model ---------------------------------------------------------------
-
-
-# Fit the model
-model <- brm(
-  formula = age_adjusted_mortality ~ gas_ppi * temperature + (1 | nuts_code),
-  data = full_data,
-  family = gaussian(),
-  iter = 2000, 
-  chains = 4
-)
-
-# Check the summary of the model
-summary(model)
-
-
-
-## From Chat GPT, needs check ----------------------------------------------
-#
-#
-## Fit the model
-#model <- brm(
-#  formula = age_adjusted_mortality ~ gas_ppi * temperature * year + (1 | nuts_code),
-#  data = your_dataframe, # replace with your actual dataframe name
-#  family = gaussian(), # adjust if needed; gaussian is for continuous outcomes
-#  iter = 2000, 
-#  chains = 4
-#)
-#
-## Check the summary of the model
-#summary(model)
-#
-#
-#nuts_3_hier <- full_data %>%
-#  dplyr::filter(nuts_level == 3)
-#
-#
-#nuts_3_hier$nuts0 <- substr(nuts_3_hier$nuts_code, 1, 2)
-#nuts_3_hier$nuts1 <- substr(nuts_3_hier$nuts_code, 1, 3)
-#nuts_3_hier$nuts2 <- substr(nuts_3_hier$nuts_code, 1, 4)
-#nuts_3_hier$nuts3 <- nuts_3_hier$nuts_code
-#
-#nuts_3_hier$datechar <- as.character(nuts_3_hier$dates)
-#
-#
-#
-## Fit the model
-#model <- brm(
-#  formula = age_adjusted_mortality ~ 1 + datechar + (1 + gas_ppi * temperature| nuts0/nuts1/nuts2/nuts3),
-#  data = nuts_3_hier, 
-#  family = gaussian(), 
-#  iter = 2000, 
-#  chains = 2
-#)
-#
-## Check the summary of the model
-#summary(model)
-
-
-
-# Gugus Input -------------------------------------------------------------
-
-
-# DATA WRANGLING ----------------------------------------------------------
-
-#just let it run through
+# Data Wrangling ----------------------------------------------------------
 
 {
 source("./project/code/07_data_merge.R")
@@ -128,30 +53,90 @@ dfpdata_nuts <- dfpdata %>%
   mutate(nuts_3 = as.factor(substr(nuts_code, 1, 5))) 
 }
 
-# Trying out some mixed models --------------------------------------------
+
+# Reduced Dataset ---------------------------------------------------------
+
+{
+  source("./project/code/07_data_merge.R")
+  
+  temp_mort_prices$dates <- as.Date(temp_mort_prices$dates)
+  
+  # combine filter conditions and avoid converting date to factor
+  data_temp <- temp_mort_prices %>% 
+    filter(nuts_level == 2  & 
+             !(dates >= as.Date("2020-03-01") & dates <= as.Date("2022-03-05"))) %>%
+    mutate(age_adjusted_mortality = age_adjusted_mortality*100000) %>%
+    group_by(nuts_code, year, month, week)  %>% 
+    mutate(date = paste(year, month, week, sep = "-")) %>%
+    select(!dates)
+  
+  breaks <- c(-Inf, 0, 5, 10, 15, 20, 25, 30, Inf)
+  labels <- c("<0", "0-5", "5-10", "10-15", "15-20", "20-25", "25-30", ">30")
+  
+  data_temp <- data_temp %>% 
+    mutate(temp_bin = cut(temperature, breaks = breaks, labels = labels, right = FALSE, include.lowest = TRUE))
+  
+  
+  pdata <- pdata.frame(data_temp, index = c("nuts_code","date"))
+  
+  pdata$lag_gas <- lag(pdata$gas_ppi, 8)
+  
+  pdata$lag_elect <- lag(pdata$elect_ppi, 8)
+  
+  pdata$temp_bin <- relevel(pdata$temp_bin, ref = "10-15")
+  
+  pdata <- pdata %>% mutate(winter = as.integer(case_when(
+    month %in% c(11, 12, 1, 2, 3) ~ "1", 
+    month %in% c(4, 5, 6, 7, 8, 9, 10) ~ "0")))
+  
+  dfpdata <- as.data.frame(pdata)
+  
+  dfpdata_nuts_2 <- dfpdata %>% 
+    mutate(country = as.factor(substr(nuts_code, 1, 2))) %>%
+    mutate(nuts_1 = as.factor(substr(nuts_code, 1, 3))) %>%
+    mutate(nuts_2 = as.factor(substr(nuts_code, 1, 4)))
 
 
-#lmer uses the same syntax as brms 
+data_reduced <- dfpdata_nuts_2 %>%
+  filter(!is.na(lag_gas),
+         !is.na(temp_bin),
+         !is.na(age_adjusted_mortality))
+}
 
-model <- brm(
-  formula = age_adjusted_mortality ~  log(lag_gas) * temp_bin + (1|country/nuts_1/nuts_2/nuts_3) + (1|date),
-  data = dfpdata_nuts, 
-  family = gaussian(), 
-  iter = 2000, 
-  chains = 4
+
+# Model 3 -----------------------------------------------------------------
+
+model3 <- brm(
+  age_adjusted_mortality ~ 
+    1 + log(lag_gas) + temp_bin + log(lag_gas):temp_bin + 
+    (1 + log(lag_gas) + temp_bin + log(lag_gas):temp_bin | country/nuts_1) +
+    (1 + log(lag_gas) + temp_bin + log(lag_gas):temp_bin | year/month),  
+  data = data_reduced, 
+  warmup = 500, 
+  iter = 1000,
+  chains = 4,
+  cores = 4,
+  seed = 1234,
+  prior = set_prior("normal(0,10)", class = "b"),
+  refresh = 1
 )
 
-summary(model)
-
-
-model3 <- brm(popular ~ 1 + sex + extrav + (1 + sex + extrav | class),  
-              data = popular2data, 
-              warmup = 1000, iter = 3000, 
-              cores = 2, chains = 2, 
-              seed = 123) #to run the model
+save(model3, file = "...") # Too large for GitHub
 
 
 
+# Model 5 -----------------------------------------------------------------
+
+model5 <- stan_glmer(
+  age_adjusted_mortality ~ 
+    1 + log(lag_gas) + temp_bin + log(lag_gas):temp_bin + 
+    (1 + log(lag_gas) + temp_bin + log(lag_gas):temp_bin | country) +
+    (1 | yearweek),  
+  data = data_reduced,
+  refresh = 1
+)
+
+# Model did not run in time
 
 
 # rstanarm ----------------------------------------------------------------
