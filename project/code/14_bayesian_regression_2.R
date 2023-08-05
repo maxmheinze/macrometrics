baecef8152e5a1a7af75c42e1f6795d0f3b07909
maxmheinze <heinzemax@icloud.com>
2023-08-05 12:06:24 +0200

    update code 14

--- project/code/14_bayesian_regression_2.R
+++ project/code/14_bayesian_regression_2.R
@@ -101,6 +101,20 @@ data_reduced <- dfpdata_nuts_2 %>%
   filter(!is.na(lag_gas),
          !is.na(temp_bin),
          !is.na(age_adjusted_mortality))
+
+rm(
+  age_adjusted_weekly_mortality, 
+  data_temp, 
+  dfpdata, 
+  dfpdata_nuts_2, 
+  pdata, 
+  prices, 
+  prices_1, 
+  temp_mort, 
+  temp_mort_date, 
+  temp_mort_prices, 
+  temperature
+)
 }
 
 
@@ -139,28 +153,114 @@ model5 <- stan_glmer(
 # Model did not run in time
 
 
-# rstanarm ----------------------------------------------------------------
 
-stan_lm(age_adjusted_mortality ~ temp_bin*log(lag_gas) + nuts_code + date, 
-        data = dfpdata_nuts)
+# Trying for faster runtime -----------------------------------------------
 
-model <- stan_lm(
-  age_adjusted_mortality ~ temp_bin*log(lag_gas) + nuts_code + date, 
-  data = dfpdata_nuts, 
-  family = gaussian(), 
-  chains = 2, 
-  iter = 200,
-  seed = 123
+{
+  source("./project/code/07_data_merge.R")
+  
+  temp_mort_prices$dates <- as.Date(temp_mort_prices$dates)
+  
+  # combine filter conditions and avoid converting date to factor
+  data_temp <- temp_mort_prices %>% 
+    filter(nuts_level == 1  & 
+             !(dates >= as.Date("2020-03-01") & dates <= as.Date("2022-03-05"))) %>%
+    mutate(age_adjusted_mortality = age_adjusted_mortality*100000) %>%
+    group_by(nuts_code, year, month, week)  %>% 
+    mutate(date = paste(year, month, week, sep = "-")) %>%
+    select(!dates)
+  
+  breaks <- c(-Inf, 5, 20, Inf)
+  labels <- c("cold", "temperate", "hot")
+  
+  data_temp <- data_temp %>% 
+    mutate(temp_bin = cut(temperature, breaks = breaks, labels = labels, right = FALSE, include.lowest = TRUE))
+  
+  
+  pdata <- pdata.frame(data_temp, index = c("nuts_code","date"))
+  
+  pdata$lag_gas <- lag(pdata$gas_ppi, 8)
+  
+  pdata$lag_elect <- lag(pdata$elect_ppi, 8)
+  
+  pdata$temp_bin <- relevel(pdata$temp_bin, ref = "temperate")
+  
+  pdata <- pdata %>% mutate(winter = as.integer(case_when(
+    month %in% c(11, 12, 1, 2, 3) ~ "1", 
+    month %in% c(4, 5, 6, 7, 8, 9, 10) ~ "0")))
+  
+  dfpdata <- as.data.frame(pdata)
+  
+  dfpdata_nuts_2 <- dfpdata %>% 
+    mutate(country = as.factor(substr(nuts_code, 1, 2))) %>%
+    mutate(nuts_1 = as.factor(substr(nuts_code, 1, 3))) %>%
+    mutate(nuts_2 = as.factor(substr(nuts_code, 1, 4)))
+  
+  
+  data_reduced_2 <- dfpdata_nuts_2 %>%
+    filter(!is.na(lag_gas),
+           !is.na(temp_bin),
+           !is.na(age_adjusted_mortality))
+  
+  rm(
+    age_adjusted_weekly_mortality, 
+    data_temp, 
+    dfpdata, 
+    dfpdata_nuts_2, 
+    pdata, 
+    prices, 
+    prices_1, 
+    temp_mort, 
+    temp_mort_date, 
+    temp_mort_prices, 
+    temperature,
+    breaks, 
+    labels
+  )
+}
+
+data_reduced_2 <- data_reduced_2 %>%
+  as_tibble() %>%
+  arrange(year, week) %>%
+  group_by(year, week) %>%
+  mutate(time_index = as.factor(cur_group_id()),
+         age_adjusted_mortality = age_adjusted_mortality / 10^5) %>%
+  ungroup() %>%
+  dplyr::select(nuts_code, country, time_index, temp_bin, lag_gas, age_adjusted_mortality)
+
+model7 <- stan_glmer(
+  age_adjusted_mortality ~ 
+    1 + log(lag_gas) + temp_bin + log(lag_gas):temp_bin + 
+    (1 + log(lag_gas) + temp_bin + log(lag_gas):temp_bin | country) +
+    (1 | time_index),  
+  data = data_reduced_2,
+  chains = 4,
+  cores = 4,
+  refresh = 1
 )
 
-model <- stan_glmer(
-  age_adjusted_mortality ~ log(lag_gas) + (1 | nuts_3/country),
-  data = dfpdata_nuts, 
-  family = gaussian(), 
-  chains = 4, 
-  iter = 100,
-  seed = 123
-)
+# # rstanarm ----------------------------------------------------------------
+# 
+# stan_lm(age_adjusted_mortality ~ temp_bin*log(lag_gas) + nuts_code + date, 
+#         data = dfpdata_nuts)
+# 
+# model <- stan_lm(
+#   age_adjusted_mortality ~ temp_bin*log(lag_gas) + nuts_code + date, 
+#   data = dfpdata_nuts, 
+#   family = gaussian(), 
+#   chains = 2, 
+#   iter = 200,
+#   seed = 123
+# )
+# 
+# model <- stan_glmer(
+#   age_adjusted_mortality ~ log(lag_gas) + (1 | nuts_3/country),
+#   data = dfpdata_nuts, 
+#   family = gaussian(), 
+#   chains = 4, 
+#   iter = 100,
+#   seed = 123
+# )
 
 
 
